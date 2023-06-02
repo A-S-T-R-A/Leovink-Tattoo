ee489995f97e1f7962b2beaa74a4dd48b0f2f4ae
Art1us <109896229+Art1us@users.noreply.github.com>
2023-06-02 14:26:41 +0300

    resolved conflicts

--- src/shared/ui/Typography/Typography.tsx
+++ src/shared/ui/Typography/Typography.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react"
-import { classNames } from "shared/lib/classNames"
+import { classNames } from "shared/lib/classNames/classNames"
 import styles from "./Typography.module.scss"
 
 export enum TypographySize {
