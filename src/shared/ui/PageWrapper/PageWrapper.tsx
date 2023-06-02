8258f82b94ba5061fcafc830dbe9d3c6c303f9d7
Art1us <109896229+Art1us@users.noreply.github.com>
2023-06-02 17:24:04 +0300

    fin

--- src/shared/ui/PageWrapper/PageWrapper.tsx
+++ src/shared/ui/PageWrapper/PageWrapper.tsx
@@ -1,16 +1,10 @@
 import { ReactNode } from "react"
 import { Typography, TypographyColor, TypographySize } from "../Typography/Typography"
 import { ArrowLeftIcon } from "../Icons"
-import { useNavigate } from "react-router-dom"
 import styles from "./PageWrapper.module.scss"
+import { AppLink } from "../AppLink/AppLink"
 
 export function PageWrapper({ children, title }: { children: ReactNode; title: string }) {
-    const navigate = useNavigate()
-
-    function clickHandler() {
-        navigate("/")
-    }
-
     return (
         <div className={styles.wrapper}>
             <div className={styles.header}>
@@ -21,9 +15,9 @@ export function PageWrapper({ children, title }: { children: ReactNode; title: s
                 >
                     {title}
                 </Typography>
-                <Typography size={TypographySize.H4} className={styles.back} onClick={clickHandler}>
+                <AppLink className={styles.back} to="/">
                     <ArrowLeftIcon /> BACK
-                </Typography>
+                </AppLink>
             </div>
 
             <div className={styles.content}>{children}</div>
