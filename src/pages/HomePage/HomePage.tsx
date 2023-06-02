b6492a736db5bc4851a1f12aea82334e40c787e9
VadimGalian <vadim.galian@gmail.com>
2023-06-02 17:43:52 +0300

    add section ui

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,14 @@
-import { Typography } from "shared/ui/Typography/Typography"
+import { Section } from "shared/ui/Section/Section"
+import { Typography, TypographyColor } from "shared/ui/Typography/Typography"
 
 export function HomePage() {
     return (
-        <div>
-            <Typography>Hello World</Typography>
-        </div>
+        <Section title="Hello">
+            <Typography color={TypographyColor.COLOR_ACCENT}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque fugiat ipsum,
+                impedit officia, ipsam, ipsa maiores iste placeat quisquam cumque rem! Excepturi
+                nobis, quas nulla aperiam id distinctio perspiciatis saepe?
+            </Typography>
+        </Section>
     )
 }
