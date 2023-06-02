3b9bad06988da143dafda639736aa18993cf7d4a
Art1us <109896229+Art1us@users.noreply.github.com>
2023-06-02 14:17:34 +0300

    fin

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,3 @@
-import { Typography } from "../../shared/ui/Typography/Typography"
-
 export function HomePage() {
-    return (
-        <Typography color="color_accent" variant="extra_small">
-            Hello World
-        </Typography>
-    )
+    return <></>
 }
