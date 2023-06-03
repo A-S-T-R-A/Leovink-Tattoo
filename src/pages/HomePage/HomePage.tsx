bef4c3137f5ffef17726f19a4751f3a12bafc198
Art1us <109896229+Art1us@users.noreply.github.com>
2023-06-03 18:18:57 +0300

    inter

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,4 @@
+import { Artists } from "widgets/Artists/Artists"
 import { Main } from "widgets/Main/Main"
 import { Portfolio } from "widgets/Portfolio/Portfolio"
 
@@ -6,6 +7,7 @@ export function HomePage() {
         <div>
             <Main />
             <Portfolio />
+            <Artists />
             <div style={{ height: "300vh" }}></div>
         </div>
     )
---
+++ src/widgets/Artists/const/data.ts
@@ -0,0 +1,37 @@
+import { IArtistData } from "../types/types"
+import image from "./imgArtist.jpg"
+
+export const data: IArtistData[] = [
+    {
+        id: 1,
+        img: image,
+        name: "Hector",
+        specialization: "Specializes in caligraphy and fine line",
+        description: `Hector's incredible talent, attention to details, and precise line work, combined with an increadibly 
+        positive attitude, make him one of the shop's most sought-after artists.`,
+    },
+    {
+        id: 2,
+        img: image,
+        name: "Vasia",
+        specialization: "Specializes in caligraphy and fine line",
+        description: `Hector's incredible talent, attention to details, and precise line work, combined with an increadibly 
+        positive attitude, make him one of the shop's most sought-after artists.`,
+    },
+    {
+        id: 3,
+        img: image,
+        name: "Petia",
+        specialization: "Specializes in caligraphy and fine line",
+        description: `Hector's incredible talent, attention to details, and precise line work, combined with an increadibly 
+        positive attitude, make him one of the shop's most sought-after artists.`,
+    },
+    {
+        id: 4,
+        img: image,
+        name: "Kolia",
+        specialization: "Specializes in caligraphy and fine line",
+        description: `Hector's incredible talent, attention to details, and precise line work, combined with an increadibly 
+        positive attitude, make him one of the shop's most sought-after artists.`,
+    },
+]
