09d37e5ecf70610863d646f06cd4fc695539d0a7
Art1us <109896229+Art1us@users.noreply.github.com>
2023-06-04 17:29:11 +0300

    fin

--- src/shared/const/faqData.ts
+++ src/shared/const/faqData.ts
@@ -28,4 +28,63 @@ export const faqData: IFaqBlock[] = [
             },
         ],
     },
+    {
+        id: 2,
+        title: "Basic",
+        questions: [
+            {
+                id: 1,
+                question: "What is the significance and meaning behind the tattoo design? ",
+                answer: "Before getting a tattoo, it's essential to consider the symbolism and personal significance of the design. Ask yourself why you want this specific tattoo and what it represents to you. Make sure the meaning aligns with your values and is something you'll be happy to carry with you for a lifetime.",
+            },
+            {
+                id: 2,
+                question: "Are you committed to the long-term implications of having a tattoo?",
+                answer: "Tattoos are permanent, so it's important to think about the long-term implications. Consider how the tattoo may affect your personal and professional life. Will it be easily visible or easy to cover up if needed? Reflect on whether you are prepared to live with the tattoo as you age and whether it aligns with your future goals and aspirations.",
+            },
+            {
+                id: 3,
+                question: "Have you thoroughly researched the tattoo artist and studio?",
+                answer: "The skill and experience of the tattoo artist are crucial factors in ensuring a satisfactory outcome. Research the artist's portfolio, read reviews from previous clients, and visit the studio to assess its cleanliness and professionalism. Take the time to find an artist whose style aligns with the design you have in mind and who has a reputation for producing quality work.",
+            },
+            {
+                id: 4,
+                question:
+                    "Have you considered the potential pain, healing process, and aftercare involved?",
+                answer: "Tattoos can be painful, and the healing process can take weeks. Consider your pain tolerance and whether you're willing to endure the discomfort. Additionally, tattoos require proper aftercare to prevent infections and ensure optimal healing. Reflect on your willingness and ability to follow the artist's instructions for aftercare to ensure the best possible outcome for your tattoo.",
+            },
+            {
+                id: 5,
+                question: "Have you thoroughly researched the tattoo artist and studio?",
+                answer: "The skill and experience of the tattoo artist are crucial factors in ensuring a satisfactory outcome. Research the artist's portfolio, read reviews from previous clients, and visit the studio to assess its cleanliness and professionalism. Take the time to find an artist whose style aligns with the design you have in mind and who has a reputation for producing quality work.",
+            },
+            {
+                id: 6,
+                question:
+                    "Have you considered the potential pain, healing process, and aftercare involved?",
+                answer: "Tattoos can be painful, and the healing process can take weeks. Consider your pain tolerance and whether you're willing to endure the discomfort. Additionally, tattoos require proper aftercare to prevent infections and ensure optimal healing. Reflect on your willingness and ability to follow the artist's instructions for aftercare to ensure the best possible outcome for your tattoo.",
+            },
+        ],
+    },
+    {
+        id: 3,
+        title: "Advanced",
+        questions: [
+            {
+                id: 1,
+                question: "What is the significance and meaning behind the tattoo design? ",
+                answer: "Before getting a tattoo, it's essential to consider the symbolism and personal significance of the design. Ask yourself why you want this specific tattoo and what it represents to you. Make sure the meaning aligns with your values and is something you'll be happy to carry with you for a lifetime.",
+            },
+            {
+                id: 2,
+                question: "Are you committed to the long-term implications of having a tattoo?",
+                answer: "Tattoos are permanent, so it's important to think about the long-term implications. Consider how the tattoo may affect your personal and professional life. Will it be easily visible or easy to cover up if needed? Reflect on whether you are prepared to live with the tattoo as you age and whether it aligns with your future goals and aspirations.",
+            },
+            {
+                id: 3,
+                question: "Have you thoroughly researched the tattoo artist and studio?",
+                answer: "The skill and experience of the tattoo artist are crucial factors in ensuring a satisfactory outcome. Research the artist's portfolio, read reviews from previous clients, and visit the studio to assess its cleanliness and professionalism. Take the time to find an artist whose style aligns with the design you have in mind and who has a reputation for producing quality work.",
+            },
+        ],
+    },
 ]
