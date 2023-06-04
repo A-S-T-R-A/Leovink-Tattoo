109e56a87f9b9795b7418faaf3567714af868316
Art1us <109896229+Art1us@users.noreply.github.com>
2023-06-04 17:19:32 +0300

    fin

--- src/shared/types/types.ts
+++ src/shared/types/types.ts
@@ -5,3 +5,15 @@ export interface ITattooImage {
     style: string
     color: string
 }
+
+interface IFaqQuestion {
+    id: number
+    question: string
+    answer: string
+}
+
+export interface IFaqBlock {
+    id: number
+    title: string
+    questions: IFaqQuestion[]
+}
