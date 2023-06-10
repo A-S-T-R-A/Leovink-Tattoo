30eef57908e3764b12a14be42005848a987785c6
Art1us <109896229+Art1us@users.noreply.github.com>
2023-06-11 00:30:22 +0300

    fin

--- admin/src/pages/PortfolioPage/ui/PortfolioPageHeader/PortfolioPageHeader.tsx
+++ admin/src/pages/PortfolioPage/ui/PortfolioPageHeader/PortfolioPageHeader.tsx
@@ -2,6 +2,7 @@ import { UploadModal } from "features/uploadTattooImage"
 import styles from "./PortfolioPageHeader.module.scss"
 import { classNames } from "shared/lib/classNames/classNames"
 import { ViewType } from "../../types/types"
+import { localStorageView } from "pages/PortfolioPage/lib/localStorageLib"
 
 export function PortfolioPageHeader({
     view,
@@ -12,18 +13,23 @@ export function PortfolioPageHeader({
     setView: (val: ViewType) => void
     triggerRefetch: () => void
 }) {
+    function changeView(view: ViewType) {
+        setView(view)
+        localStorageView.set(view)
+    }
+
     return (
         <div className={styles.header}>
             <UploadModal triggerRefetch={triggerRefetch} />
             <div className={styles.viewToggle}>
                 <button
-                    onClick={() => setView("table")}
+                    onClick={() => changeView("table")}
                     className={classNames("", { [styles.selectedView]: view === "table" })}
                 >
                     Table
                 </button>
                 <button
-                    onClick={() => setView("icons")}
+                    onClick={() => changeView("icons")}
                     className={classNames("", { [styles.selectedView]: view === "icons" })}
                 >
                     Icons
