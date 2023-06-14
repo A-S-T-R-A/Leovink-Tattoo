e6e315b5369201e114021643475ffe2191c392cb
VadimGalian <vadim.galian@gmail.com>
2023-06-14 16:03:54 +0300

    update

--- front/src/widgets/Main/Main.tsx
+++ front/src/widgets/Main/Main.tsx
@@ -1,7 +1,8 @@
 import styles from "./Main.module.scss"
 import leovinkTitle from "./const/leovinkLogo.png"
-import leovinkCaption from "./const/leovCaption.png"
+import leovinkCaption from "./const/leovCaption.webp"
 import leovinkHorns from "./const/leovinkHorns.png"
+import { ScrollIcon } from "shared/ui/ScrollIcon/ScrollIcon"
 
 export function Main() {
     return (
@@ -11,6 +12,7 @@ export function Main() {
                 <img src={leovinkTitle} alt="leovink tattoo" className={styles.title} />
                 <img src={leovinkCaption} alt="tattoo & piercing" className={styles.caption} />
             </div>
+            <ScrollIcon className={styles.scroll} />
         </header>
     )
 }
