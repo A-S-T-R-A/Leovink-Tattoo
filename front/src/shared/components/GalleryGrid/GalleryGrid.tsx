54ca2056ac3ac9b0af380124ed3cdaac0b8d5465
VadimGalian <vadim.galian@gmail.com>
2023-06-14 18:48:43 +0300

    new portfolio gallery

---
+++ front/src/shared/components/GalleryGrid/GalleryGrid.tsx
@@ -0,0 +1,38 @@
+import type { ITattooImage } from "shared/types/types"
+//import { data as d } from "shared/const/data"
+import styles from "./GalleryGrid.module.scss"
+import { EyeIcon } from "shared/ui/Icons"
+import { classNames } from "shared/lib/classNames/classNames"
+
+interface IGalleryGrid {
+    data: ITattooImage[]
+    maxHeight?: string
+    onClick?: (index: number) => void
+}
+
+export function GalleryGrid({ data, onClick, maxHeight = "auto" }: IGalleryGrid) {
+    return (
+        <div
+            className={classNames(styles.container, {
+                [styles.withGradient]: maxHeight !== "auto",
+            })}
+            style={{ maxHeight }}
+        >
+            <div className={styles.gridContainer}>
+                {data.map((item, index) => {
+                    const { id, img } = item
+                    return (
+                        <div
+                            key={id}
+                            className={styles.imgContainer}
+                            onClick={() => onClick?.(index)}
+                        >
+                            <img src={img} alt="" />
+                            <EyeIcon className={styles.eye} />
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
