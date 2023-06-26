cdea960b7a8b73c0b8d71d2bfd17496e004e0549
VadimGalian <vadim.galian@gmail.com>
2023-06-26 14:53:59 +0300

    Update Sidebar.tsx

--- admin/src/widgets/Sidebar/ui/Sidebar.tsx
+++ admin/src/widgets/Sidebar/ui/Sidebar.tsx
@@ -11,7 +11,7 @@ export function Sidebar() {
     return (
         <div className={styles.wrapper}>
             <div className={styles.container}>
-                {routes.map((item, index) => {
+                {routes.map(item => {
                     if (item.private && user === "none") return null
                     if (item.allowedRoles && !item.allowedRoles?.includes(user)) return null
 
@@ -26,90 +26,6 @@ export function Sidebar() {
                         </Link>
                     )
                 })}
-
-                {/* {user !== "none" && (
-                    <Link
-                        className={`${styles.link} ${
-                            section === "Portfolio" && styles.currentLink
-                        }`}
-                        onClick={() => clickHandler("Portfolio")}
-                        to="/portfolio"
-                    >
-                        Portfolio
-                    </Link>
-                )}
-                {user !== "none" && (
-                    <Link
-                        className={`${styles.link} ${section === "Steps" && styles.currentLink}`}
-                        onClick={() => clickHandler("Steps")}
-                        to="/steps"
-                    >
-                        Steps
-                    </Link>
-                )}
-                {user !== "none" && (
-                    <Link
-                        className={`${styles.link} ${section === "Services" && styles.currentLink}`}
-                        onClick={() => clickHandler("Services")}
-                        to="/services"
-                    >
-                        Services
-                    </Link>
-                )}
-                {user !== "none" && (
-                    <Link
-                        className={`${styles.link} ${section === "Reviews" && styles.currentLink}`}
-                        onClick={() => clickHandler("Reviews")}
-                        to="/reviews"
-                    >
-                        Reviews
-                    </Link>
-                )}
-                {user !== "none" && (
-                    <Link
-                        className={`${styles.link} ${section === "Artists" && styles.currentLink}`}
-                        onClick={() => clickHandler("Artists")}
-                        to="/artists"
-                    >
-                        Artists
-                    </Link>
-                )}
-                {user !== "none" && (
-                    <Link
-                        className={`${styles.link} ${section === "FAQ" && styles.currentLink}`}
-                        onClick={() => clickHandler("FAQ")}
-                        to="/faq"
-                    >
-                        FAQ
-                    </Link>
-                )}
-                {user === "dev" && (
-                    <Link
-                        className={`${styles.link} ${section === "Contacts" && styles.currentLink}`}
-                        onClick={() => clickHandler("Contacts")}
-                        to="/contacts"
-                    >
-                        *Contacts
-                    </Link>
-                )}
-                {user === "dev" && (
-                    <Link
-                        className={`${styles.link} ${section === "Other" && styles.currentLink}`}
-                        onClick={() => clickHandler("Other")}
-                        to="/other"
-                    >
-                        *Other
-                    </Link>
-                )}
-                {user === "dev" && (
-                    <Link
-                        className={`${styles.link} ${section === "Test" && styles.currentLink}`}
-                        onClick={() => clickHandler("Test")}
-                        to="/test"
-                    >
-                        *Test
-                    </Link>
-                )} */}
             </div>
         </div>
     )
