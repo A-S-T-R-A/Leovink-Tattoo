7db33c1d084e6b5436dc69e322b106528c013e5c
Art1us <109896229+Art1us@users.noreply.github.com>
2023-07-05 18:23:07 +0300

    fin

--- admin/src/features/authByGoogle/const/emailsWhitelist.ts
+++ admin/src/features/authByGoogle/const/emailsWhitelist.ts
@@ -2,6 +2,6 @@ import { IEmailsWhiteList } from "../types/types"
 
 export const emailsWhitelist: IEmailsWhiteList = {
     admin: ["masterleovinktattoo@gmail.com"],
-    dev: ["artur0503915024@gmail.com", "vadim.galian@gmail.com"],
+    dev: ["artur0503915024@gmail.com", "vadim.galian@gmail.com", "valerophob@gmail.com"],
     other: ["leovinktattoo@gmail.com"],
 }
