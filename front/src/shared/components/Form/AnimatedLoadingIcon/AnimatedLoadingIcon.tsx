ad4bd553b53879e23beedac59743e5ddc6b05be8
Art1us <109896229+Art1us@users.noreply.github.com>
2023-07-05 10:27:33 +0300

    fin

---
+++ front/src/shared/components/Form/AnimatedLoadingIcon/AnimatedLoadingIcon.tsx
@@ -0,0 +1,9 @@
+import styles from "./AnimatedLoadingIcon.module.css"
+import { AiOutlineLoading3Quarters } from "react-icons/ai"
+
+function AnimatedLoadingIcon({ className }: { className?: string }) {
+    /* @ts-ignore */
+    return <AiOutlineLoading3Quarters className={`${styles.icon} ${className}`} />
+}
+
+export default AnimatedLoadingIcon
