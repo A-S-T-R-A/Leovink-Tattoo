20916c8876e1f27b7c3072b0f3d24de34330ee3c
VadimGalian <vadim.galian@gmail.com>
2023-07-07 11:48:28 +0300

    update

--- admin/src/shared/ui/CustomNotifications/ui/Alert/alertConstructor.tsx
+++ admin/src/shared/ui/CustomNotifications/ui/Alert/alertConstructor.tsx
@@ -9,24 +9,25 @@ export function alertConstructor(
     variant: string
 ) {
     const alertContainerEl = document.createElement("div")
-    const messageEl = document.createElement("p")
+    const messageContainerEl = document.createElement("div")
     const strongTitleEl = document.createElement("strong")
+    const messageEl = document.createElement("p")
     const closeIconEl = crossIcon(styles.cross)
     const iconEl = icon(styles.icon)
 
     strongTitleEl.textContent = text + " "
-    messageEl.append(strongTitleEl)
-    messageEl.textContent += message
+    messageEl.textContent = message
+    messageContainerEl.append(strongTitleEl)
+    messageContainerEl.append(messageEl)
 
     alertContainerEl.classList.add(styles.container)
     alertContainerEl.classList.add(styles[variant])
-    messageEl.classList.add(styles.message)
+    messageContainerEl.classList.add(styles.message)
 
     alertContainerEl.style.transition = `transform ${TRANSITION_TIME}ms ease`
 
     alertContainerEl.append(iconEl)
-    //alertContainerEl.append(strongTitleEl)
-    alertContainerEl.append(messageEl)
+    alertContainerEl.append(messageContainerEl)
     alertContainerEl.append(closeIconEl)
 
     document.body.append(alertContainerEl)
@@ -45,7 +46,7 @@ export function alertConstructor(
         alertContainerEl.classList.add(styles.appended)
     })
 
-    //cleanup(DISAPPEARING_DELAY)
+    cleanup(DISAPPEARING_DELAY)
 
     closeIconEl.onclick = () => cleanup()
 }
