d8b3bd9b443c3199c108571a60fe2022f7517863
Art1us <109896229+Art1us@users.noreply.github.com>
2023-07-10 14:34:46 +0300

    t

--- front/.netlify/functions-internal/chunks/index.1332a7c9.a0d19c09.mjs
+++
@@ -1,6 +0,0 @@
-const link = "_link_1j7es_1";
-const styles = {
-	link: link
-};
-
-export { styles as s };
--- front/.netlify/functions-internal/chunks/portfolio.dfc1b224.8089ad6f.mjs
+++
@@ -1,6 +0,0 @@
-const btn = "_btn_cc9jp_1";
-const styles = {
-	btn: btn
-};
-
-export { styles as s };
