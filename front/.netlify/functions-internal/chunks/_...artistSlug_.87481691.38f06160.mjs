8f88f60a8c69a7dd76e238ee23f4ba46b435debc
Art1us <109896229+Art1us@users.noreply.github.com>
2023-07-10 14:19:12 +0300

    upd

---
+++ front/.netlify/functions-internal/chunks/_...artistSlug_.87481691.38f06160.mjs
@@ -0,0 +1,99 @@
+const wrapper$1 = "_wrapper_1n0cu_1";
+const blur = "_blur_1n0cu_25";
+const container$3 = "_container_1n0cu_35";
+const logoContainer$2 = "_logoContainer_1n0cu_55";
+const navbar = "_navbar_1n0cu_65";
+const styles$7 = {
+	wrapper: wrapper$1,
+	blur: blur,
+	container: container$3,
+	logoContainer: logoContainer$2,
+	navbar: navbar
+};
+
+const list = "_list_6krsr_1";
+const undreline = "_undreline_6krsr_51";
+const item$1 = "_item_6krsr_75";
+const styles$6 = {
+	list: list,
+	undreline: undreline,
+	item: item$1
+};
+
+const container$2 = "_container_173xu_2";
+const socialIcons = "_socialIcons_173xu_9";
+const navList = "_navList_173xu_13";
+const styles$5 = {
+	container: container$2,
+	socialIcons: socialIcons,
+	navList: navList
+};
+
+const socials$1 = "_socials_1n8i0_1";
+const item = "_item_1n8i0_7";
+const styles$4 = {
+	socials: socials$1,
+	item: item
+};
+
+const languages$1 = "_languages_1losp_1";
+const dropdownContainer = "_dropdownContainer_1losp_8";
+const dropdown = "_dropdown_1losp_8";
+const language = "_language_1losp_1";
+const styles$3 = {
+	languages: languages$1,
+	dropdownContainer: dropdownContainer,
+	dropdown: dropdown,
+	language: language
+};
+
+const icon = "_icon_ryah6_1";
+const container$1 = "_container_ryah6_9";
+const burgerOpen = "_burgerOpen_ryah6_31";
+const hamburger = "_hamburger_ryah6_31";
+const overlay = "_overlay_ryah6_68";
+const navOpen = "_navOpen_ryah6_81";
+const wrapper = "_wrapper_ryah6_86";
+const btn = "_btn_ryah6_115";
+const languages = "_languages_ryah6_119";
+const styles$2 = {
+	icon: icon,
+	container: container$1,
+	burgerOpen: burgerOpen,
+	hamburger: hamburger,
+	overlay: overlay,
+	navOpen: navOpen,
+	wrapper: wrapper,
+	btn: btn,
+	languages: languages
+};
+
+const container = "_container_7jrle_1";
+const logoContainer$1 = "_logoContainer_7jrle_12";
+const locationContainer = "_locationContainer_7jrle_22";
+const contactsContainer = "_contactsContainer_7jrle_23";
+const followContainer = "_followContainer_7jrle_24";
+const header = "_header_7jrle_30";
+const text = "_text_7jrle_37";
+const socials = "_socials_7jrle_44";
+const styles$1 = {
+	container: container,
+	logoContainer: logoContainer$1,
+	locationContainer: locationContainer,
+	contactsContainer: contactsContainer,
+	followContainer: followContainer,
+	header: header,
+	text: text,
+	socials: socials
+};
+
+const logoContainer = "_logoContainer_nte08_1";
+const logo = "_logo_nte08_1";
+const styles = {
+	logoContainer: logoContainer,
+	logo: logo
+};
+
+const Layout = '';
+
+export { styles$4 as a, styles$3 as b, styles$5 as c, styles$2 as d, styles$7 as e, styles$1 as f, styles as g, styles$6 as s };
---
+++ front/.netlify/functions-internal/chunks/_...artistSlug_.b11b3c3e.956ad7aa.mjs
@@ -0,0 +1,36 @@
+const container$1 = "_container_199ls_1";
+const prev = "_prev_199ls_12";
+const next = "_next_199ls_13";
+const cross = "_cross_199ls_14";
+const swiper = "_swiper_199ls_76";
+const slide = "_slide_199ls_81";
+const slideContainer = "_slideContainer_199ls_88";
+const img = "_img_199ls_95";
+const descriptionContainer = "_descriptionContainer_199ls_101";
+const styles$1 = {
+	container: container$1,
+	prev: prev,
+	next: next,
+	cross: cross,
+	swiper: swiper,
+	slide: slide,
+	slideContainer: slideContainer,
+	img: img,
+	descriptionContainer: descriptionContainer
+};
+
+const container = "_container_1hbpa_1";
+const withGradient = "_withGradient_1hbpa_4";
+const gridContainer = "_gridContainer_1hbpa_8";
+const item = "_item_1hbpa_17";
+const eye = "_eye_1hbpa_28";
+const styles = {
+	container: container,
+	withGradient: withGradient,
+	gridContainer: gridContainer,
+	item: item,
+	eye: eye,
+	"break": "_break_1hbpa_68"
+};
+
+export { styles as a, styles$1 as s };
