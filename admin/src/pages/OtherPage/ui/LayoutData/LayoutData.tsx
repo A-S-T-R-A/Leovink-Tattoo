c7e4da5bb1951616d33d65dc5814ad03e8fc0b8b
VadimGalian <vadim.galian@gmail.com>
2023-07-11 16:06:14 +0300

    Update LayoutData.tsx

--- admin/src/pages/OtherPage/ui/LayoutData/LayoutData.tsx
+++ admin/src/pages/OtherPage/ui/LayoutData/LayoutData.tsx
@@ -190,7 +190,6 @@ export function LayoutData({
                 onDiscardClick={discardClickHandler}
             >
                 <div className={styles.wrapperModal}>
-                    {" "}
                     <div className={styles.modalContainer}>
                         <p>Footer List</p>
                         <Input
