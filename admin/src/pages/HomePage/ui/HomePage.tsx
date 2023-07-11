bef33a01e2cee44ee8caa5a9b66053b7f34d1b37
Art1us <109896229+Art1us@users.noreply.github.com>
2023-07-11 13:07:29 +0300

    fin

--- admin/src/pages/HomePage/ui/HomePage.tsx
+++ admin/src/pages/HomePage/ui/HomePage.tsx
@@ -7,8 +7,8 @@ export function HomePage() {
             <div className={styles.filter} />
             <img src={leovinkHorns} alt="Tattoo parlor in Chisinau" className={styles.horns} />
             <div className={styles.textContainer}>
-                <p className={styles.first}>Welcome to the Leovink Tattoo admin panel</p>
-                <p className={styles.second}>Please login to proceed</p>
+                <p className={styles.first}>Welcome to Leovink Tattoo</p>
+                <p className={styles.second}>Please login to use admin panel</p>
             </div>
         </div>
     )
