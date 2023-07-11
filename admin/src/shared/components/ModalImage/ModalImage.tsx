bc430beb0ae1695c7318abff4b09ce62e9f36344
Art1us <109896229+Art1us@users.noreply.github.com>
2023-07-11 15:10:52 +0300

    inter

--- admin/src/shared/components/ModalImage/ModalImage.tsx
+++ admin/src/shared/components/ModalImage/ModalImage.tsx
@@ -3,6 +3,7 @@ import { PlusIcon } from "shared/ui/Icons"
 import { Modal } from "shared/ui/Modal"
 
 import styles from "./ModalImage.module.scss"
+import { classNames } from "shared/lib/classNames/classNames"
 
 export function ModalImage({ url, className }: { url: string; className?: string }) {
     const [isOpen, setIsOpen] = useState(false)
@@ -20,7 +21,12 @@ export function ModalImage({ url, className }: { url: string; className?: string
                 </div>
                 <img src={url} className={styles.img} />
             </Modal>
-            <img src={url} alt="" className={className} onClick={() => setIsOpen(true)} />
+            <img
+                src={url}
+                alt=""
+                className={classNames(styles.img, {}, [className])}
+                onClick={() => setIsOpen(true)}
+            />
         </>
     )
 }
